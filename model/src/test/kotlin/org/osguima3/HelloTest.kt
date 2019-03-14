b44d3bbb20e877146842adf12ebcf01eb1ff8ec8
Gustavo Rodriguez Liccioni <gustavo.liccioni@n26.com>
2019-03-14 12:13:20 +0100

    first commit for developing plugin
    (cherry picked from commit e36ca4d1bbceeab6d5d6ac6e4780b93d5297dbfe)

--- model/src/test/kotlin/org/osguima3/HelloTest.kt
+++ model/src/test/kotlin/org/osguima3/HelloTest.kt
@@ -1,7 +1,5 @@
 package org.osguima3
 
-import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class HelloTest {
 
--- plugin/src/test/kotlin/org/osguima3/HelloMojoTest.kt
+++ plugin/src/test/kotlin/org/osguima3/HelloMojoTest.kt
@@ -1,8 +1,30 @@
 package org.osguima3
 
+import org.apache.maven.plugin.testing.AbstractMojoTestCase
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
-class HelloMojoTest {
 
+class HelloMojoTest : AbstractMojoTestCase() {
+
+
+    protected override fun setUp() {
+        super.setUp()
+    }
+
+
+    protected override fun tearDown() {
+        super.tearDown()
+    }
+
+
+    @Test
+    fun testSomething() {
+        val pom = getTestFile("src/test/resources/unit/project-to-test/pom.xml")
+//        assertNotNull(pom)
+        assertTrue(pom.exists())
+
+        val myMojo = lookupMojo("hello", pom) as HelloMojo
+//        assertNotNull(myMojo)
+        myMojo.execute()
+    }
 }
