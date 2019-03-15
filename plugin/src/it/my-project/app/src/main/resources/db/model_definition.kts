7770b2c785174dedb25dbbc31afefd7fef31b4bd
Oscar Guillén <oscar.guillen@n26.com>
2019-03-15 14:58:00 +0100

    Moved model definition to kts file

--- plugin/src/it/my-project/app/src/main/resources/db/model_definition.kts
+++ plugin/src/it/my-project/app/src/main/resources/db/model_definition.kts
@@ -1,12 +1,12 @@
-package org.company.myproject.model.types
-
+import org.company.myproject.model.types.CompanyCreationDate
+import org.company.myproject.model.types.CompanyName
 import org.osguima3.jooqdsl.model.ModelDefinition
 
-class MyModelDefinition : ModelDefinition({
+ModelDefinition {
     tables {
         table("company") {
             "name" withTinyType CompanyName::class
             "creation_date" withTinyType CompanyCreationDate::class
         }
     }
-})
+}
