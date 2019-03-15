f6e51264aaa4314bcea19aada562d13c9996fe5b
Oscar Guillén <oscar.guillen@n26.com>
2019-03-15 12:16:26 +0100

    Merged withInstantTinyType into withTinyType

--- model/src/main/kotlin/org/osguima3/jooqdsl/model/ModelDefinition.kt
+++ model/src/main/kotlin/org/osguima3/jooqdsl/model/ModelDefinition.kt
@@ -5,7 +5,7 @@ import org.osguima3.jooqdsl.model.definition.ModelContext
 /**
  * This class is used to define the model to be used in generated Jooq classes.
  */
-class ModelDefinition(private val configure: ModelContext.() -> Unit) {
+open class ModelDefinition(private val configure: ModelContext.() -> Unit) {
 
     fun accept(visitor: DefinitionVisitor) = visitor.visit(configure)
 }
