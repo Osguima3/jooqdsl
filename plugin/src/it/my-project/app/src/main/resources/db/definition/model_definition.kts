628abc0ae1b9e2e090b26cd268f474e2244672b7
Oscar Guillén <oscar.guillen@n26.com>
2019-03-18 19:19:59 +0100

    Added support for UUID

--- plugin/src/it/my-project/app/src/main/resources/db/definition/model_definition.kts
+++ plugin/src/it/my-project/app/src/main/resources/db/definition/model_definition.kts
@@ -1,9 +1,15 @@
-import org.company.myproject.model.types.*
+import org.company.myproject.model.types.CompanyCreationDate
+import org.company.myproject.model.types.CompanyEmployees
+import org.company.myproject.model.types.CompanyId
+import org.company.myproject.model.types.CompanyIndustry
+import org.company.myproject.model.types.CompanyName
+import org.company.myproject.model.types.LegalEntityType
 import org.osguima3.jooqdsl.model.ModelDefinition
 
 ModelDefinition {
     tables {
         table("company") {
+            field("id", CompanyId::class)
             field("name", CompanyName::class)
             field("creation_date", CompanyCreationDate::class)
             field("employees", CompanyEmployees::class)
